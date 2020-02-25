f8dc701997958289ee3f90e655e980ca4c5d4c96
alexivar <alexivar65@gmail.com>
2020-02-25 01:19:53 +0000

    fixed style on page

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -3,11 +3,11 @@ import React from 'react';
 function Homepage() {
   return (
     <div className="container-fluid">
-      <div className="jumbotron jumbotron-fluid">
+      <div className="jumbotron jumbotron-fluid" id="welcomeJumbo">
         <div className="container">
-          <h1 className="display-4">Flight planning Pro</h1>
-            <p className="lead">
-              Big things are comming up!
+          <h1 className="display-4">Flight planning</h1>
+            <p className="lead" id="welcomeJumboLead">
+              _____________________________________________
 
             </p>
         </div>
